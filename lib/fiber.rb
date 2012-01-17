4430cfb91bc09e2eaad55876e513dfd78f638712
al_skipp <al_skipp@fastmail.fm>
2012-01-17 08:23:57 +0000

    Initial commit from gist

---
+++ lib/fiber.rb
@@ -0,0 +1,114 @@
+class FiberError < StandardError; end
+
+class Fiber
+  attr_accessor :name
+  def initialize &block
+    raise ArgumentError, 'new Fiber requires a block' unless block_given?
+    @block = block
+    
+    @yield_sem = Dispatch::Semaphore.new(0)
+    @resume_sem = Dispatch::Semaphore.new(0)
+    
+    @fiber_queue = Dispatch::Queue.new "#{__id__}"
+    @fiber_queue.async {@yield_sem.wait}
+    Fiber[@fiber_queue.label]= self
+
+    execute_block
+  end
+  
+  private
+  def execute_block
+    @completed = false
+    @fiber_queue.async do 
+      @result ||= @block.call(*@args)
+      unless self == Fiber[:root_fiber]
+        Fiber.delete_fiber(@fiber_queue.label)
+        @block = nil # when @block becomes nil the fiber is dead
+      end
+      @completed = true
+      @resume_sem.signal
+      @result
+    end
+  end
+  
+  public
+  def resume *args
+    raise FiberError, 'dead fiber called' if @block.nil?
+    # raise FiberError, 'double resume' if @has_transferred == true
+    
+    execute_block if @completed # root fiber never dies
+    
+    if @args.nil?
+      @args = args
+    else
+      @result = args.size > 1 ? args : args.first
+    end
+
+    @yield_sem.signal
+    @resume_sem.wait unless @has_transferred == :complete
+    @result
+  end
+
+  # yield is called from inside the block passed to Fiber.new and executes within the @fiber_queue
+  def yield *args
+    @result = args.size > 1 ? args : args.first
+    @resume_sem.signal
+    @yield_sem.wait
+    @result
+  end
+  
+  def transfer *args
+    fiber = Fiber.current # the fiber in whose context the transfer call was made. Should be given a yield call to suspend execution of its block
+    @transferred_from = fiber
+
+    if Fiber[:root_fiber] == fiber # check for transfer to root fiber
+      self.resume *args
+    else
+      resume = lambda do |previous_fiber|
+        @has_transferred = :complete if self == previous_fiber && !previous_fiber.nil? 
+        self.resume *args
+      end
+          
+      fiber.instance_eval do
+        @has_transferred = true
+        self.yield *resume.call(@transferred_from)      
+        @resume_sem.wait and @has_transferred = nil if @has_transferred == :complete
+      end
+    end
+  end
+  
+  def alive?
+    !@block.nil?
+  end
+  
+  def inspect
+    "#<#{self.class}:0x#{self.object_id.to_s(16)}>"
+  end
+  
+  def self.current
+    Fiber[Dispatch::Queue.current.label] || Fiber[:root_fiber]
+  end
+  
+  def self.yield *args
+    raise FiberError, "can't yield from root fiber" unless fiber = Fiber[Dispatch::Queue.current.label]
+    fiber.yield *args
+  end
+  
+  # class methods to get, set and delete fiber references
+  # wrapped with a serial queue for safe multi-threaded use
+  def self.[] fiber_id
+    @@fibers_queue.sync { return @@__fibers__[fiber_id] }
+  end
+  
+  def self.[]= fiber_id, fiber
+    @@fibers_queue.sync { @@__fibers__[fiber_id]= fiber }
+  end
+  
+  def self.delete_fiber fiber_id
+    @@fibers_queue.sync { @@__fibers__.delete(fiber_id) }
+  end
+  
+  @@__fibers__ = {} # create class hash to enable look-up of individual fibers when using 'Fiber.yield'
+  @@fibers_queue = Dispatch::Queue.new('fibers_queue') # create serial queue to access class hash
+  Fiber[:root_fiber]= Fiber.new { |*args| args.size > 1 ? args : args.first } # create root fiber. Default behaviour is to return arguments it receives
+end
